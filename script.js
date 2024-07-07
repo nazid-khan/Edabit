295ea1d5bb1b170e58869ee1e6e33604a0d463b3
nazid <nazidk773@gmail.com>
2024-07-07 18:59:44 +0530

    return index of given items

--- script.js
+++ script.js
@@ -323,15 +323,53 @@
 
 
 
-// Given an index and an array, return the value of the array with the given index.
+// // Given an index and an array, return the value of the array with the given index.
 
-function valueAt(arr, i) {
-	return arr[Math.floor(i)]
+// function valueAt(arr, i) {
+// 	return arr[Math.floor(i)]
+// }
+
+// console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2));      //➞ 6
+// console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2));     //➞ 5
+// console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2));   //➞ 4
+
+
+
+
+
+
+// Create a function that finds the index of a given item.
+
+function search(arr, item) {
+	return arr.indexOf(item)
 }
 
-console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2));      //➞ 6
-console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2));     //➞ 5
-console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2));   //➞ 4
+console.log(search([1, 5, 3], 5)); //➞ 1
+console.log(search([9, 8, 3], 3)); //➞ 2
+console.log(search([1, 2, 3], 4)); //➞ -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
