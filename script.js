4bf49c345c925935d5230862dcc07c091ecc7034
nazid <nazidk773@gmail.com>
2024-07-08 13:09:27 +0530

    object how much a person needs to spend

--- script.js
+++ script.js
@@ -406,23 +406,34 @@
 
 
 
-// Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
+// // Create a function that takes two numbers num1, num2, and an array arr and returns an array containing all the numbers in arr greater than num1 and less than num2.
 
-function arrBetween(num1, num2, arr) {
-	return arr.filter(number => number > num1 && number < num2);
-  }
+// function arrBetween(num1, num2, arr) {
+// 	return arr.filter(number => number > num1 && number < num2);
+//   }
 
-console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7]));      //➞ [5, 4, 7]
-console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]));   //➞ [8, 6]
-console.log(arrBetween(7, 32, [1, 2, 3, 78]));           //➞ []
-
-
+// console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7]));      //➞ [5, 4, 7]
+// console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]));   //➞ [8, 6]
+// console.log(arrBetween(7, 32, [1, 2, 3, 78]));           //➞ []
 
 
 
 
 
 
+
+// The 50-30-20 strategy is a simple way to budget, which involves spending 50% of after-tax income on needs, 30% after tax income on wants, and 20% after-tax income on savings or paying off debt.
+// Given the after-tax income as ati, what you are supposed to do is to make a function that will return an object that shows how much a person needs to spend on needs, wants, and savings.
+
+function fiftyThirtyTwenty(ati) {
+	return  { "Needs": ati * 50 / 100, 
+			  "Wants": ati * 30 / 100, 
+			  "Savings": ati * 20 / 100 }
+}
+
+console.log(fiftyThirtyTwenty(10000)); //➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
+console.log(fiftyThirtyTwenty(50000));  //➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
+console.log(fiftyThirtyTwenty(13450));  //➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
 
 
 
