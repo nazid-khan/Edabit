a17b6706c690c7756c689d7cc2d1da12a4156fde
nazid <nazidk773@gmail.com>
2024-07-09 12:24:40 +0530

    Adjectives Total

--- script.js
+++ script.js
@@ -545,15 +545,36 @@
 
 
 
-// Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+// // Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
 
-function checkSquareAndCube(arr) {
-    return Math.sqrt(arr[0]) === Math.cbrt(arr[1]);
+// function checkSquareAndCube(arr) {
+//     return Math.sqrt(arr[0]) === Math.cbrt(arr[1]);
+// }
+
+// console.log(checkSquareAndCube([4, 8])); //➞ true
+// console.log(checkSquareAndCube([16, 48])); //➞ false
+// console.log(checkSquareAndCube([9, 27])); //➞ true
+
+
+
+
+
+// You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given an object of insults, return the total amount of insults used.
+
+function totalAmountAdjectives(obj) {
+	return Object.keys(obj).length
 }
 
-console.log(checkSquareAndCube([4, 8])); //➞ true
-console.log(checkSquareAndCube([16, 48])); //➞ false
-console.log(checkSquareAndCube([9, 27])); //➞ true
+console.log(totalAmountAdjectives({ a: "moron" }));         //➞ 1
+console.log(totalAmountAdjectives({ a: "idiot", b: "idiot", c: "idiot" }));     //➞ 3
+console.log(totalAmountAdjectives({ a: "moron", b: "scumbag", c: "moron", d: "dirtbag" }));    //➞ 4
+
+
+
+
+
+
+
 
 
 
