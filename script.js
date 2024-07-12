e29d033254591e9f9e7173bea20a031df55d7b5c
nazid <nazidk773@gmail.com>
2024-07-12 23:05:55 +0530

    add question

--- script.js
+++ script.js
@@ -559,15 +559,15 @@
 
 
 
-// You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given an object of insults, return the total amount of insults used.
+// // You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given an object of insults, return the total amount of insults used.
 
-function totalAmountAdjectives(obj) {
-	return Object.keys(obj).length
-}
+// function totalAmountAdjectives(obj) {
+// 	return Object.keys(obj).length
+// }
 
-console.log(totalAmountAdjectives({ a: "moron" }));         //➞ 1
-console.log(totalAmountAdjectives({ a: "idiot", b: "idiot", c: "idiot" }));     //➞ 3
-console.log(totalAmountAdjectives({ a: "moron", b: "scumbag", c: "moron", d: "dirtbag" }));    //➞ 4
+// console.log(totalAmountAdjectives({ a: "moron" }));         //➞ 1
+// console.log(totalAmountAdjectives({ a: "idiot", b: "idiot", c: "idiot" }));     //➞ 3
+// console.log(totalAmountAdjectives({ a: "moron", b: "scumbag", c: "moron", d: "dirtbag" }));    //➞ 4
 
 
 
